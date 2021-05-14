2bc8cacba72cdf40380c0973f0e1fefbd853e1e5
Marcel Rehkemper <marcel.rehkemper@gmx.de>
2021-05-14 15:34:47 +0200

    experimenting with code

--- TableTopCrucible.App.WpfTestApp/App.xaml.cs
+++ TableTopCrucible.App.WpfTestApp/App.xaml.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.EventLog;
+using ReactiveUI;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
+using Splat.Microsoft.Extensions.Logging;
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TableTopCrucible.App.Shared;
 
 namespace TableTopCrucible.App.WpfTestApp
 {
@@ -13,6 +22,22 @@ namespace TableTopCrucible.App.WpfTestApp
     /// </summary>
     public partial class App : Application
     {
-        
+        public IServiceProvider Container { get; private set; }
+        public App()
+        {
+            var host = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.UseMicrosoftDependencyResolver();
+                    var resolver = Locator.CurrentMutable;
+                    resolver.InitializeSplat();
+                    resolver.InitializeReactiveUI();
+
+                    services.
+                    DependencyBuilder.BuildDependencyProvider();
+                });
+
+        }
     }
 }
---
+++ TableTopCrucible.App.WpfTestApp/ViewModels/MainVM.cs
@@ -0,0 +1,23 @@
+﻿using Splat;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TableTopCrucible.App.WpfTestApp.ViewModels
+{
+    interface IMain
+    {
+        static IMain()
+        {
+            Locator.CurrentMutable.Register(() => new MainVM(), typeof(IMain));
+
+        }
+    }
+    class MainVM : IMain
+    {
+        public MainVM()
+        {
+        }
+    }
+}
