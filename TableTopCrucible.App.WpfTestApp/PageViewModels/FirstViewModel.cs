86153e39813a30c3245001fc548d47b921bb66f7
Marcel Rehkemper <marcel.rehkemper@gmx.de>
2021-05-19 20:22:37 +0200

    comand example added

--- TableTopCrucible.App.WpfTestApp/PageViewModels/FirstViewModel.cs
+++ TableTopCrucible.App.WpfTestApp/PageViewModels/FirstViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 using Splat;
 
 using System;
 using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 
 using TableTopCrucible.App.WpfTestApp.Pages;
@@ -13,6 +17,33 @@ namespace TableTopCrucible.App.WpfTestApp.PageViewModels
     public class FirstViewModel : ReactiveObject, IRoutableViewModel, IActivatableViewModel
     {
         public string UrlPathSegment => "first";
+
+        public FirstViewModel()
+        {
+            ;
+            this.WhenActivated((CompositeDisposable disposables) =>
+            {
+
+                SetText = ReactiveCommand.Create(
+                        () => Value = "setByCommand",
+                        this.WhenAnyValue(vm => vm.Value).Select(string.IsNullOrWhiteSpace),
+                        RxApp.TaskpoolScheduler
+                    ).DisposeWith(disposables);
+
+                AddText = ReactiveCommand.Create(
+                        (string suffix) => Value += suffix,
+                        this.WhenAnyValue(vm => vm.SuffixFills),
+                        RxApp.TaskpoolScheduler
+                    ).DisposeWith(disposables);
+            });
+        }
+
+        [Reactive]
+        public string Value { get; set; }
+        [Reactive]
+        public bool SuffixFills { get; set; }
+        public ReactiveCommand<Unit, string> SetText { get; private set; }
+        public ReactiveCommand<string, string> AddText { get; private set; }
 
         public IScreen HostScreen { get; }
         public ViewModelActivator Activator { get; } = new ViewModelActivator();
--- TableTopCrucible.App.WpfTestApp/Pages/FirstView.xaml.cs
+++ TableTopCrucible.App.WpfTestApp/Pages/FirstView.xaml.cs
@@ -4,6 +4,7 @@ using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,6 +32,29 @@ namespace TableTopCrucible.App.WpfTestApp.Pages
             {
                 this.OneWayBind(ViewModel, vm => vm.UrlPathSegment, v => v.PathTextBlock.Text)
                     .DisposeWith(disposables);
+
+                this.Bind(ViewModel, vm => vm.Value, v => v.DemoTextBox.Text)
+                    .DisposeWith(disposables);
+
+                this.OneWayBind(ViewModel, vm => vm.SuffixFills, v => v.suffixFills.Text, v => v.ToString())
+                    .DisposeWith(disposables);
+
+                this.BindCommand(this.ViewModel, vm => vm.SetText, v => v.setText)
+                    .DisposeWith(disposables);
+
+                this.BindCommand(
+                        this.ViewModel,
+                        vm => vm.AddText,
+                        v => v.addText,
+                        this.WhenAnyValue(v => v.parameter.Text))
+                    .DisposeWith(disposables);
+
+                this.parameter
+                    .WhenAnyValue(v => v.Text)
+                    .Select(txt => !string.IsNullOrWhiteSpace(txt))
+                    .BindTo(ViewModel, vm => vm.SuffixFills)
+                    .DisposeWith(disposables);
+                
             });
 
         }
