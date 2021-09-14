8516d3b0260ddd3991757880e813cb78ec13da27
Marcel Rehkemper <marcel.rehkemper@gmx.de>
2021-09-14 15:05:36 +0200

    valuetype unittest added

---
+++ Core/TableTopCrucible.Core.ValueTypes.Tests/FileExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TableTopCrucible.Core.ValueTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableTopCrucible.Core.ValueTypes.Tests
+{
+    [TestFixture]
+    public class FileExtensionTests
+    {
+        [Test]
+        public void GetFileTypeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
