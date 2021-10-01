ebc31f9cdf1771a745b1f3bdfaa44c31c48b9bb3
Marcel Rehkemper <marcel.rehkemper@gmx.de>
2021-10-02 01:32:30 +0200

    jobqueue content added

--- Core/TableTopCrucible.Core.Wpf.Engine/UserControls/ViewModels/JobQueueVm.cs
+++ Core/TableTopCrucible.Core.Wpf.Engine/UserControls/ViewModels/JobQueueVm.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DynamicData;
+using DynamicData.Binding;
 using ReactiveUI;
 using TableTopCrucible.Core.DependencyInjection.Attributes;
+using TableTopCrucible.Core.Jobs.Progression.Services;
+using TableTopCrucible.Core.Jobs.Progression.ValueTypes;
 using TableTopCrucible.Core.ValueTypes;
 using TableTopCrucible.Core.Wpf.Engine.Models;
 using TableTopCrucible.Core.Wpf.Engine.ValueTypes;
@@ -16,8 +20,25 @@ namespace TableTopCrucible.Core.Wpf.Engine.UserControls.ViewModels
     {}
     public class JobQueueVm:ReactiveObject, IActivatableViewModel, IJobQueue
     {
+        private readonly IProgressTrackingService _progressTrackingService;
         public ViewModelActivator Activator { get; } = new();
         public Name Title => (Name) "Job Queue";
         public SidebarWidth Width => null;
+
+        //public ObservableCollectionEx
+
+        public JobQueueVm(IProgressTrackingService progressTrackingService)
+        {
+            _progressTrackingService = progressTrackingService;
+
+            //this.WhenActivated(()=>new []
+            //{
+            //    _progressTrackingService
+            //        .TrackerList
+            //        .Connect()
+            //        .Filter(job=>job.JobStateChanges == JobState.Done)
+            //        .BindTo()
+            //});
+        }
     }
 }
